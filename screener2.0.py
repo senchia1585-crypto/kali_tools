0df10ce8dd80d1fbc0e6ec5c8ab66c04edfd0b45
sen chia <senchia1585@gmail.com>
2026-04-13 20:47:02 +0800

    Add files via upload

--- screener2.0.py
+++ screener2.0.py
@@ -1,7 +1,7 @@
 import requests as r
 import time as t 
 import random 
-import selenium
+#import selenium
 id_list = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
@@ -23,60 +23,63 @@ while True:
             with open(f_name,"r",encoding="utf-8") as read: # second open for open inside !
               print(f"try to open txt document for screening打开中: {f_name}\n Your target目标:\n{target}")
               for l in read: # second for use to screen inside dict and dict2.0 # read line by line
-               path=l.strip() # all path 
-              if not path or path.startswith("#"):
-                 continue
-              aim=f"{target.rstrip('/')}/{path.lstrip('/')}" # auto settle /  ex:rstrp--->google.com//... cut ///  lstrip-->///google.com cut /one
-              header={f"User-agent":random.choice(id_list)} # random to choice one of the ip
-            try:
-              print(f"try to screnning..... : {aim}",end="\r")
-              t.sleep(random.uniform(0.1,0.6))
-              answer=r.get(aim , headers=header, timeout=5, allow_redirects=False) # if false will not follow again ! # get with headers so that let u become a human
-              if answer.status_code==200:
-                 print(f"[+ success ! ]found: \n{aim}\n")
-                 print(f"content:\n{answer.text[:80].strip()}---")# write down only 80 word to file!
-                 try:
-                    with open("answer.txt","a",encoding="utf-8") as wr:
-                       print(f"[+ success]found!:\n{path}")
-                       wr.write(f"态状 status找到!found:\n{aim}")
-                 except Exception as e1:
-                    print(f"[fail ! pls check]\n:{e1}")
-              elif answer.status_code ==403:
-                 print(f"{answer.status_code}\n没权限!")
-                 print(f"To:{target}")    
-                 try:
-                     with open("answer.txt","a",encoding="utf-8") as wr2:
-                       print(f"写入!:路径:\n{aim}")
-                       wr2.write(f"{aim}\n")
-                 except Exception as e2:
-                    print(f"pls check:\n{e2}")
-              elif answer.status_code==429:
-                 print(f"[!!!!!!warning !]:\n {answer.status_code}")
-                 print("ip may refuse connection pls wait for 10s ...")
-                 t.sleep(10)
-              elif answer.status_code==401:
-                 print(f"{answer.status_code}\n发现好料!:\n{aim}")
-                 with open("answer.txt","a",encoding="utf-8") as ww:
-                    ww.write(f"[401!]:\n{aim}")
-                 try:
-                     with open("answer.txt","a",encoding="utf-8") as wr3:
-                       print(f"写入!:{aim}")
-                       wr3.write(f"{aim}")
-                 except Exception as e2:
-                    print(f"{e2}")
-              elif answer.status_code==405:
-                 print(f"有好料但不给:\n{answer.status_code}")
-              elif answer.status_code==500:
-                 print(f"[!!!有问题]\n{answer.status_code}")
-              else: # 404
-                 pass
-            except Exception as get_wrong:
-                print(f"pls check\n:{get_wrong}")
+                 path=l.strip() # all path 
+                 if not path or path.startswith("#"):
+                  continue
+                 aim=f"{target.rstrip('/')}/{path.lstrip('/')}" # auto settle /  ex:rstrp--->google.com//... cut ///  lstrip-->///google.com cut /one
+                 header={f"User-agent":random.choice(id_list)} # random to choice one of the ip
+                 try: # second tryhave to inside in second for so that it can be detect
+                  print(f"try to screnning..... : {aim}",end="\r")
+                  t.sleep(random.uniform(0.1,0.6))
+                  time=t.localtime()
+                  print(f"{time.tm_hour}:{time.tm_min}:{time.tm_sec}--->try to screening: {aim}",end="\r")
+                  answer=r.get(aim , headers=header, timeout=5, allow_redirects=False) # if false will not follow again ! # get with headers so that let u become a human
+                  if answer.status_code==200:
+                     print(f"[+ success ! ]found: \n{aim}\n")
+                     print(f"content:\n{answer.text[:80].strip()}---")# write down only 80 word to file!
+                     try: 
+                        with open("answer.txt","a",encoding="utf-8") as wr:
+                           print(f"[+ success]found!:\n{path}")
+                           wr.write(f"态状 status找到!found:\n{aim}")
+                           wr.write(f"{answer.text[:80].strip()}") # try to save 80 word to file!
+                     except Exception as e1:
+                        print(f"[fail ! pls check]\n:{e1}")
+                  elif answer.status_code ==403:
+                     print(f"{answer.status_code}\n没权限!")
+                     print(f"To:{target}")    
+                     try:
+                           with open("answer.txt","a",encoding="utf-8") as wr2:
+                            print(f"写入!:路径:\n{aim}")
+                            wr2.write(f"{aim}\n")
+                     except Exception as e2:
+                        print(f"pls check:\n{e2}")
+                  elif answer.status_code==429:
+                     print(f"[!!!!!!warning !]:\n {answer.status_code}")
+                     print("ip may refuse connection pls wait for 10s ...")
+                     t.sleep(10)
+                  elif answer.status_code==401:
+                     print(f"{answer.status_code}\n发现好料!:\n{aim}")
+                     with open("answer.txt","a",encoding="utf-8") as ww:
+                        ww.write(f"[401!]:\n{aim}")
+                     try:
+                           with open("answer.txt","a",encoding="utf-8") as wr3:
+                            print(f"写入!:{aim}")
+                            wr3.write(f"{aim}")
+                     except Exception as e2:
+                        print(f"{e2}")
+                  elif answer.status_code==405:
+                     print(f"有好料但不给:\n{answer.status_code}")
+                  elif answer.status_code==500:
+                     print(f"[!!!有问题]\n{answer.status_code}")
+                  else: # 404
+                     pass
+                 except Exception as get_wrong:
+                  print(f"pls check\n:{get_wrong}")
         except Exception as e_final:
            print(f"may have problem!!\n{e_final}")
         except FileNotFoundError as no_found:
            print(f"cannot find the file!{no_found}")
-print(f"screen finish! pls check!---->answer.txt")
+print(f"screen finish! pls check!---->answer.txt or check ur code and try again !")
                        
 
 
